ed60637582e3617001cdbf4030102761b6d576c9
thegouge <thegouge42@gmail.com>
2019-10-12 23:00:34 -0700

    re-initialize for like the third time

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-
-import IonicVue from "@ionic/vue";
-import "@ionic/core/css/core.css";
-import "@ionic/core/css/ionic.bundle.css";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
-Vue.use(IonicVue);
-
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: h => h(App)
 }).$mount("#app");
