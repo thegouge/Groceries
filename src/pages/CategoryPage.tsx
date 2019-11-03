243fb85425aef53017e56c503f3e9eb907dc19ec
thegouge <thegouge42@gmail.com>
2019-11-02 19:11:12 -0700

    made CategoryPage somewhat functional

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 import {withRouter, RouteComponentProps} from "react-router-dom";
-import {IonPage, IonContent} from "@ionic/react";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonMenuButton,
+  IonTitle,
+  IonButtons,
+  IonButton,
+} from "@ionic/react";
+
+import {testList} from "../lib/defaultData";
+import Category from "../components/category";
 
 interface queryProps {
   id: string;
 }
 
-const CategoryPage = ({match}: RouteComponentProps<queryProps>) => (
-  <IonPage>
-    <IonContent>Hello from {match.params.id}</IonContent>
-  </IonPage>
-);
+const CategoryPage = ({match}: RouteComponentProps<queryProps>) => {
+  const selectedCategory = testList.find(
+    (category) => `${category.id}` === match.params.id
+  );
+
+  if (!selectedCategory) {
+    return <div>How the hell are you even here?</div>;
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonMenuButton slot="start" />
+          <IonTitle slot="start">{selectedCategory.name}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>Hello from {selectedCategory.name}</IonContent>
+    </IonPage>
+  );
+};
 
 export default withRouter(CategoryPage);
