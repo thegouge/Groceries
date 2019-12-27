2912b3454fa73334d96011f484d4a5424d7a9de5
thegouge <thegouge42@gmail.com>
2019-12-26 19:43:19 -0800

    started initializing capacitor storage

--- src/context/categoryContext.tsx
+++ src/context/categoryContext.tsx
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from "react";
 
+import {Plugins} from "@capacitor/core";
+
 import {defaultCategoriesList} from "../lib/defaultData";
 
 import {CategoryClass} from "../lib/interfaces";
+
+const {Storage} = Plugins;
 
 interface catContextProps {
   categoriesList: CategoryClass[];
@@ -18,6 +22,22 @@ const CategoryProvider = (props: any) => {
     const category: CategoryClass = {...categoryToAdd, id: nextId};
 
     setCategoriesList([...categoriesList, category]);
+  };
+
+  const saveCategories = () => {
+    Storage.set({
+      key: "categories",
+      value: JSON.stringify(categoriesList),
+    });
+  };
+
+  const loadCategories = async () => {
+    const data = await Storage.get({key: "categories"});
+    if (data.value === null) {
+      return;
+    } else {
+      setCategoriesList(JSON.parse(data.value));
+    }
   };
 
   return (
